57fd179fe573f59c8aba5028b592c401e82c8774
Riscoh Tech <riscohfundzgmail.com>
2025-10-30 09:39:20 +0100

    make some correction on createStudent component and validate the form to display a message

--- src/components/CreateStudent.jsx
+++ src/components/CreateStudent.jsx
@@ -18,8 +18,9 @@ const CreateStudent = () => {
   const studentSchemaValidation = object({
   firstName: string().required("Firstname is required"),
   lastName: string().required("Lastname is required"),
-  age: string().required("Age"),
-  phone: string().required().min(11, "Phone Number must be 11 xters"),
+  age: string().required("Age is rewuired"),
+  phone: string() .required("Phone number is required").matches(
+  /^234\d{10}$/, "Phone number must start with 234 and be 13 digits total"),
 });
   const {
     register, handleSubmit,  formState: { errors },} = useForm({
@@ -44,9 +45,7 @@ const CreateStudent = () => {
       if (saveResponse.status === 201) {
         setLoading(false);
         console.log("Record saved successfully");
-        setTimeout(() => {
           navigate("/");
-        }, 9000);
       } 
     } catch (err) {
       console.log("There is error ", err);
@@ -92,6 +91,7 @@ const CreateStudent = () => {
                   </div>
                   {errors.firstName && (
                     <span className="text-red-500">
+                    {errors.firstName.message}
                     </span>
                   )}
                 </div>
@@ -111,6 +111,11 @@ const CreateStudent = () => {
                       className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                     />
                   </div>
+                  {errors.lastName && (
+                    <span className="text-red-500">
+                    {errors.lastName.message}
+                    </span>
+                  )}
                 </div>
               </div>
 
@@ -125,11 +130,16 @@ const CreateStudent = () => {
                   <div className="mt-2">
                     <input
                       id="age"
-                      type="text"
+                      type="number"
                       {...register("age")}
                       className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                     />
                   </div>
+                   {errors.age && (
+                    <span className="text-red-500">
+                    {errors.age.message}
+                    </span>
+                  )}
                 </div>
 
                 <div className="sm:col-span-3">
@@ -142,11 +152,17 @@ const CreateStudent = () => {
                   <div className="mt-2">
                     <input
                       id="phone"
-                      type="text"
+                      type="number"
                       {...register("phone")}
                       className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                     />
                   </div>
+                   {errors.phone && (
+                    <span className="text-red-500">
+                    {errors.phone.message}
+
+                    </span>
+                  )}
                 </div>
               </div>
             </div>
@@ -162,7 +178,7 @@ const CreateStudent = () => {
             </button>
             </Link>
             <button
-              type="submit"
+              type="onSubmit"
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Save
